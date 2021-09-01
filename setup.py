62b758a8e13191b470129ecdab54a5da79c55140
masashis2021PBL <89332649+masashis2021PBL@users.noreply.github.com>
2021-09-01 14:22:36 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,20 +8,27 @@ import config
 
 def install_packages ():
 	config.logger.info ("Update packages")
-	subprocess.call (["apt-get", "-y", "update"])
+	subprocess.call (["sudo", "apt-get", "-y", "update"])
 
 	config.logger.info ("Install packages")
-	subprocess.call (["apt-get", "-y", "install", "python3-pip"])
-	subprocess.call (["pip3", "install", "--upgrade", "setuptools"])
-	subprocess.call (["pip3", "install", "--upgrade", "pip"])
-	subprocess.call (["pip3", "install", "rpi.gpio"])
-	subprocess.call (["pip3", "install", "pyserial"])
-	subprocess.call (["pip3", "install", "ntpdate"])
-	subprocess.call (["pip3", "install", "git-core"])
-	subprocess.call (["pip3", "install", "i2c-tools"])
-	subprocess.call (["pip3", "install", "libi2c-dev"])
-	subprocess.call (["pip3", "install", "python-smbus"])
+	subprocess.call (["sudo", "apt-get", "-y", "install", "python3-pip"])
+	subprocess.call (["sudo", "pip3", "install", "--upgrade", "setuptools"])
+	subprocess.call (["sudo", "pip3", "install", "--upgrade", "pip"])
+	subprocess.call (["sudo", "pip3", "install", "rpi.gpio"])
+	subprocess.call (["sudo", "pip3", "install", "pyserial"])
+	subprocess.call (["sudo", "pip3", "install", "ntpdate"])
+	subprocess.call (["sudo", "pip3", "install", "git-core"])
+	subprocess.call (["sudo", "pip3", "install", "i2c-tools"])
+	subprocess.call (["sudo", "pip3", "install", "libi2c-dev"])
+	subprocess.call (["sudo", "pip3", "install", "subversion"])
+	subprocess.call (["sudo", "pip3", "install", "python-smbus"])
 
+	config.logger.info ("Install deep-learning packages")
+	subprocess.call (["sudo", "apt-get", "install", "libatlas-base-dev"])
+	subprocess.call (["sudo", "pip3", "--default-timeout=100", "install", "tensorflow"])
+	subprocess.call (["sudo", "pip3", "--default-timeout=100", "install", "keras"])
+	subprocess.call (["sudo", "apt-get", "install", "libatlas-base-dev"])
+	subprocess.call (["sudo", "git", "clone", "https://github.com/fchollet/deep-learning-models.git"])
 
 
 ########## main () ##########
