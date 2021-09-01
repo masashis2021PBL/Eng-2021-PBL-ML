e573d3dc11ba32e71b1089a5ebf23caf2b58256e
masashis2021PBL <89332649+masashis2021PBL@users.noreply.github.com>
2021-09-01 15:27:06 +0900

    Add files via upload
    Install the Machine Learning Environment to RPi3 Automatically.

--- setup.py
+++ setup.py
@@ -7,28 +7,35 @@ import subprocess
 import config
 
 def install_packages ():
+	subprocess.call (["cd", ~/"])
 	config.logger.info ("Update packages")
 	subprocess.call (["sudo", "apt-get", "-y", "update"])
 
 	config.logger.info ("Install packages")
 	subprocess.call (["sudo", "apt-get", "-y", "install", "python3-pip"])
+	subprocess.call (["sudo", "apt-get", "-y", "install", "python-dev"])
+	subprocess.call (["sudo", "apt-get", "-y", "install", "build-essenntial"])
 	subprocess.call (["sudo", "pip3", "install", "--upgrade", "setuptools"])
 	subprocess.call (["sudo", "pip3", "install", "--upgrade", "pip"])
 	subprocess.call (["sudo", "pip3", "install", "rpi.gpio"])
 	subprocess.call (["sudo", "pip3", "install", "pyserial"])
 	subprocess.call (["sudo", "pip3", "install", "ntpdate"])
-	subprocess.call (["sudo", "pip3", "install", "git-core"])
 	subprocess.call (["sudo", "pip3", "install", "i2c-tools"])
 	subprocess.call (["sudo", "pip3", "install", "libi2c-dev"])
 	subprocess.call (["sudo", "pip3", "install", "subversion"])
 	subprocess.call (["sudo", "pip3", "install", "python-smbus"])
+	subprocess.call (["sudo", "pip3", "install", "jupyter"])
+	subprocess.call (["sudo", "apt-get", "-y", "install", "python3-matplotlib"])
+	subprocess.call (["sudo", "apt-get", "-y", "install", "python3-numpy"])
+	subprocess.call (["sudo", "apt-get", "-y", "install", "git"])
 
 	config.logger.info ("Install deep-learning packages")
-	subprocess.call (["sudo", "apt-get", "install", "libatlas-base-dev"])
+	subprocess.call (["sudo", "apt-get", "-y", "install", "libatlas-base-dev"])
 	subprocess.call (["sudo", "pip3", "--default-timeout=100", "install", "tensorflow"])
 	subprocess.call (["sudo", "pip3", "--default-timeout=100", "install", "keras"])
-	subprocess.call (["sudo", "apt-get", "install", "libatlas-base-dev"])
+	subprocess.call (["sudo", "apt-get", "-y", "install", "libatlas-base-dev"])
 	subprocess.call (["sudo", "git", "clone", "https://github.com/fchollet/deep-learning-models.git"])
+	subprocess.call (["wget", "-P", "~/deep-learning-models", "https://github.com/masashis2021PBL/Eng-2021-PBL-ML/raw/main/elephant.jpg"])
 
 
 ########## main () ##########
